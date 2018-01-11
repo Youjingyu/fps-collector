9cbe64560bcc30fcea3f843204cb73e37d247a49
jingyu16 <jingyu16@staff>
2018-01-11 15:55:40 +0800

    demo

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -1,9 +1,20 @@
 import { collectAnimationFps } from '../../index';
+import { collectScrollFps } from '../../index';
 
 const collector = new collectAnimationFps({
-  duration: 8000
+  duration: 5000
 });
 collector.start();
 collector.on('end', function (data) {
+  console.log(data);
+});
+
+const scrollCollector = new collectScrollFps({
+  threshold: 2
+});
+scrollCollector.start();
+const span = document.getElementById('test');
+scrollCollector.on('end', function (data) {
+  span.textContent = data.fps + ';' + data.speed + ';' + data.quickScroll
   console.log(data);
 });
